ef7eaf251f8c9745707a6f45e949f0416fcaafa6
Edward Shanahan <edwardshanahan79@gmail.com>
2024-07-20 14:15:26 +0100

    Render Planent List

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
+import NavbarItem from "../NavbarItem/NavbarItem";
 import NavbarOverlay from "../NavbarOverlay/NavbarOverlay";
 import "./Navbar.scss";
 import { useState } from "react";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
+
+  const planetList = [
+    {
+      name: "mercury",
+      color: "#DEF4FC",
+    },
+    {
+      name: "venus",
+      color: "#F7CC7F",
+    },
+    {
+      name: "earth",
+      color: "#545BFE",
+    },
+    {
+      name: "mars",
+      color: "#FF6A45",
+    },
+    {
+      name: "jupiter",
+      color: "#ECAD7A",
+    },
+    {
+      name: "saturn",
+      color: "#FCCB6B",
+    },
+    {
+      name: "uranus",
+      color: "#65F0D5",
+    },
+    {
+      name: "neptune",
+      color: "#497EFA",
+    },
+  ];
 
   const handleToggleMenu = () => {
     if (!isActive) {
@@ -18,6 +54,12 @@ const Navbar = () => {
       <div className="Navbar__logo">THE PLANETS</div>
 
       <NavbarOverlay isActive={isActive} />
+
+      <ul className="Navbar__list">
+        {planetList.map((planet, index) => {
+          return <NavbarItem planet={planet} key={index} />;
+        })}
+      </ul>
 
       <div
         onClick={handleToggleMenu}
--- src/components/NavbarItem/NavbarItem.jsx
+++ src/components/NavbarItem/NavbarItem.jsx
@@ -1,1 +1,7 @@
-const NavbarItem = () => {};
+import "./NavbarItem.scss";
+
+const NavbarItem = ({ planet }) => {
+  return <li className="Navbar__item">{planet.name}</li>;
+};
+
+export default NavbarItem;
