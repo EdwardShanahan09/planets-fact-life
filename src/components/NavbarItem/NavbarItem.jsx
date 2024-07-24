06f157447b977b7da7f1faafa956e1c531aee389
Edward Shanahan <edwardshanahan79@gmail.com>
2024-07-24 17:09:48 +0100

    Style Navbar Link

--- src/components/NavbarItem/NavbarItem.jsx
+++ src/components/NavbarItem/NavbarItem.jsx
@@ -1,7 +1,18 @@
 import "./NavbarItem.scss";
+import IconChevron from "../../assets/icons/icon-chevron.svg";
 
 const NavbarItem = ({ planet }) => {
-  return <li className="Navbar__item">{planet.name}</li>;
+  return (
+    <li className="Navbar__item">
+      <span>
+        <span className="Navbar__planet"></span>{" "}
+        <a className="Navbar__link" href="#">
+          {planet.name}
+        </a>{" "}
+      </span>
+      <img src={IconChevron} alt="Chevron Icon" />
+    </li>
+  );
 };
 
 export default NavbarItem;
