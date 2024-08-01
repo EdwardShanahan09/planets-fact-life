806fd1fa75b8901653869caaaa21aca4bc72c469
Edward Shanahan <edwardshanahan79@gmail.com>
2024-08-01 17:53:09 +0100

    Add border bottom to Tab Buttons

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,8 +1,9 @@
 import "./TabButton.scss";
 
 const TabButton = ({ text }) => {
+  console.log(text);
   return (
-    <button style={{ borderBottom: "1px solid #419EBB" }} className="TabButton">
+    <button style={{ borderBottom: "2px solid #419EBB" }} className="TabButton">
       {text}
     </button>
   );
